0b5893921b0353e2170e7c8eb24dad26e9f401fc
kevanultroneous <dev21.ultroneous@gmail.com>
2022-10-19 11:07:27 +0530

    api

---
+++ src/api/api.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+export const Login = () => {
+    return false
+}
+export const ChangePassword = () => {
+    return false
+}
+export const ForgetPassword = () => {
+    return false
+}
