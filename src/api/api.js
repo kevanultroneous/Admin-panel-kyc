1be9f981c80367e66106839abe0bfedea27a75a1
kevanultroneous <dev21.ultroneous@gmail.com>
2022-10-31 11:09:40 +0530

    api link  change and add

--- src/api/api.js
+++ src/api/api.js
@@ -1,54 +1,56 @@
 /* eslint-disable prettier/prettier */
 import axios from "axios"
+import { api_link } from "src/utils/defaults"
+
 
 export const LoginApi = (data) => {
-    return axios.post(`http://localhost:8000/api/serviceprovider/signin`, data)
+    return axios.post(`${api_link}api/serviceprovider/signin`, data)
 }
 export const ChangePasswordApi = (data, token) => {
-    return axios.post(`http://localhost:8000/api/serviceprovider/changePassword`, data, {
+    return axios.post(`${api_link}api/serviceprovider/changePassword`, data, {
         headers: { 'Authorization': `Bearer ${token}` }
     })
 }
 export const ForgetPassword = (data) => {
-    return axios.post(`http://localhost:8000/api/serviceprovider/forgetPassword`, data)
+    return axios.post(`${api_link}api/serviceprovider/forgetPassword`, data)
 }
 export const ResetPassoword = (data, token) => {
-    return axios.post(`http://localhost:8000/api/serviceprovider/resetPassword`, data, {
+    return axios.post(`${api_link}api/serviceprovider/resetPassword`, data, {
         headers: { 'Authorization': `Bearer ${token}` }
     })
 }
 export const OTPverify = (data, token) => {
-    return axios.post(`http://localhost:8000/api/serviceprovider/verifyOtp`, data, {
+    return axios.post(`${api_link}api/serviceprovider/verifyOtp`, data, {
         headers: { 'Authorization': `Bearer ${token}` }
     })
 }
 export const getAllCustomer = () => {
-    return axios.get(`http://localhost:8000/api/admin/getallcustomers`, {
+    return axios.get(`${api_link}api/admin/getallcustomers`, {
         headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
     })
 }
 export const getAllServiceProvider = () => {
-    return axios.get(`http://localhost:8000/api/admin/getAllServiceProviders`, {
+    return axios.get(`${api_link}api/admin/getAllServiceProviders`, {
         headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
     })
 }
 export const setBlockOrUnblockUser = (data) => {
-    return axios.post(`http://localhost:8000/api/admin/blockuser`, data, {
+    return axios.post(`${api_link}api/admin/blockuser`, data, {
         headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
     })
 }
 export const customerSearch = (data) => {
-    return axios.post(`http://localhost:8000/api/admin/search`, data, {
+    return axios.post(`${api_link}api/admin/search`, data, {
         headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
     })
 }
 export const viewInDetailCustomer = (id) => {
-    return axios.post(`http://localhost:8000/api/admin/getAllCustomers`, { id: id }, {
+    return axios.post(`${api_link}api/admin/getAllCustomers`, { id: id }, {
         headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
     })
 }
 export const deleteReview = (id) => {
-    return axios.post(`http://localhost:8000/api/serviceprovider/deleteReview`, { id: id }, {
+    return axios.post(`${api_link}api/serviceprovider/deleteReview`, { id: id }, {
         headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
     })
 }
---
+++ src/utils/defaults.js
@@ -0,0 +1,2 @@
+/* eslint-disable prettier/prettier */
+export const api_link = `http://192.168.1.28:8000/`
