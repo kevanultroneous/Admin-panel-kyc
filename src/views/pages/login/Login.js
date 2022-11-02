61bf03bb08b4ea077358cbeb7606bdb249d6af9a
kevanultroneous <dev21.ultroneous@gmail.com>
2022-11-02 18:53:38 +0530

    login changes

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -36,7 +36,7 @@ const Login = () => {
 
   useEffect(() => {
     if (localStorage.getItem("token")) {
-      navigate('/customers')
+      navigate('/')
     }
   })
 
@@ -52,7 +52,7 @@ const Login = () => {
         if (r.data.data != null && (r.data.token !== null || r.data.token !== "")) {
           toast.success("Login successfully !")
           localStorage.setItem('token', r.data.token)
-          navigate('/customers')
+          navigate('/')
         }
       }).catch((e) => {
         if (e.response) {
